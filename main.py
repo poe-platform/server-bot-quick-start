50960bbcdad37cd89e98997f25cbe4242f240b67
Anmol Singh <asingh@quora.com>
2023-05-11 17:40:03 -0700

    add gitpod setup file and also add tempkey to unblock the server

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from fastapi_poe.samples.catbot import CatBot
 from fastapi_poe import run
 
-run(CatBot())
+# Add your Poe API key here. You can go to https://poe.com/create_bot?api=1 to generate one.
+POE_API_KEY = "meow" * 8
+
+run(CatBot(), api_key=POE_API_KEY)
