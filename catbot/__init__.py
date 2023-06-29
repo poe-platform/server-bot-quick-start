6d3d508d15a1116aafa1a7a48c6b93c1ec89a35c
Anmol Singh <asingh@quora.com>
2023-06-29 15:56:46 -0700

    fix precommit errors

--- catbot/__init__.py
+++ catbot/__init__.py
@@ -12,7 +12,7 @@ import asyncio
 import json
 from typing import AsyncIterable
 
-from fastapi_poe import PoeBot, run
+from fastapi_poe import PoeBot
 from fastapi_poe.types import (
     ContentType,
     QueryRequest,
