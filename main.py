b7a739657e045a11597f928e28ab47f40be71f05
Ikk Anmol Singh Hundal <5581466+anmolsingh95@users.noreply.github.com>
2023-07-15 17:16:09 -0700

    minor changes to main.py (move poe api key into the stub and add instructions for using modal secrets) (#29)

--- main.py
+++ main.py
@@ -28,19 +28,22 @@ bot = EchoBot()
 # of the form (botname1 vs botname2)
 # bot = BattleBot()
 
-# Optionally add your Poe API key here. You can go to https://poe.com/create_bot?api=1 to generate
-# one. We strongly recommend adding this key for a production bot to prevent abuse,
-# but the starter example disables the key check for convenience.
-# POE_API_KEY = ""
-# app = make_app(bot, api_key=POE_API_KEY)
-
-# specific to hosting with modal.com
+# The following is setup code that is required to host with modal.com
 image = Image.debian_slim().pip_install_from_requirements("requirements.txt")
+# Rename "poe-bot-quickstart" to your preferred app name.
 stub = Stub("poe-bot-quickstart")
 
 
 @stub.function(image=image)
 @asgi_app()
 def fastapi_app():
+    # Optionally, add your Poe API key here:
+    # 1. You can go to https://poe.com/create_bot?api=1 to generate an API key.
+    # 2. We strongly recommend using a key for a production bot to prevent abuse,
+    # but the starter example disables the key check for convenience.
+    # 3. You can also store your API key on modal.com and retrieve it in this function
+    # by following the instructions at: https://modal.com/docs/guide/secrets
+    # POE_API_KEY = ""
+    # app = make_app(bot, api_key=POE_API_KEY)
     app = make_app(bot, allow_without_key=True)
     return app
