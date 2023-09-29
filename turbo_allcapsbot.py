d8f033c274a3a554b2e483413fb48ecba39f612d
Ikk Anmol Singh Hundal <5581466+anmolsingh95@users.noreply.github.com>
2023-09-28 17:36:36 -0700

    replace chatgpt with gpt3.5 (#51)
    Co-authored-by: Anmol Singh <asingh@quora.com>

--- turbo_allcapsbot.py
+++ turbo_allcapsbot.py
@@ -1,6 +1,6 @@
 """
 
-Sample bot that wraps ChatGPT but makes responses use all-caps.
+Sample bot that wraps GPT-3.5-Turbo but makes responses use all-caps.
 
 """
 from __future__ import annotations
@@ -17,10 +17,10 @@ from fastapi_poe.types import (
 )
 
 
-class ChatGPTAllCapsBot(PoeBot):
+class GPT35TurboAllCapsBot(PoeBot):
     async def get_response(self, query: QueryRequest) -> AsyncIterable[PartialResponse]:
-        async for msg in stream_request(query, "ChatGPT", query.access_key):
+        async for msg in stream_request(query, "GPT-3.5-Turbo", query.access_key):
             yield msg.model_copy(update={"text": msg.text.upper()})
 
     async def get_settings(self, setting: SettingsRequest) -> SettingsResponse:
-        return SettingsResponse(server_bot_dependencies={"ChatGPT": 1})
+        return SettingsResponse(server_bot_dependencies={"GPT-3.5-Turbo": 1})
