f8858a8e06fbd15355c95a24381de21cd022a24d
Hui Kang Tong <you@example.com>
2024-04-28 19:38:29 -0700

    delete volumes

--- delete_volumes.py
+++ delete_volumes.py
@@ -1,19 +1,15 @@
 import json
 import subprocess
-import time
-from datetime import datetime, timezone
 
-result = subprocess.run(
-    ["modal", "nfs", "list", "--json"], capture_output=True, text=True
-)
+command = "modal app list --json > app.json"
+subprocess.run(command, shell=True, capture_output=True, text=True)
 
-for volume in json.loads(result.stdout):
-    if not volume["Name"].startswith("vol-u-"):
-        continue
-    dt = datetime.strptime(volume["Created at"], "%Y-%m-%d %H:%M:%S.%f%z")
-    dt_utc = dt.astimezone(timezone.utc)
-    created_time = (dt_utc - datetime(1970, 1, 1, tzinfo=timezone.utc)).total_seconds()
+with open("app.json") as f:
+    srr = f.read()
 
-    if int(time.time()) - created_time > 24 * 60 * 60:
-        # you can only delete individual files, but not the nfs
-        print(volume)
+for opp in json.loads(srr):
+    if opp['Name'].startswith("vol-u-"):
+        print(opp['Name'])
+        print(opp['State'])
+        command = f"modal app stop {opp['App ID']}"
+        subprocess.run(command, shell=True, capture_output=True, text=True)
