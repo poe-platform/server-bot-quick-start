2dd09b58e664a58e7efc29e7df1c194df1d243de
nhuber <nhuber@quora.com>
2024-11-26 17:35:34 -0800

    Update python_agent.py
    removing test messages

--- python_agent.py
+++ python_agent.py
@@ -2,10 +2,6 @@
 
 BOT_NAME="PythonAgent"; modal deploy --name $BOT_NAME bot_${BOT_NAME}.py; curl -X POST https://api.poe.com/bot/fetch_settings/$BOT_NAME/$POE_ACCESS_KEY
 
-Test message:
-download and save wine dataset
-list directory
-
 """
 
 from __future__ import annotations
